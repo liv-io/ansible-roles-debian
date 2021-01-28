d4ce77245b709fbe20287ae34ad334d9796a323e
l <l@liv.io>
2021-01-28 22:54:10 +0100

    Declutter CREDITS file

--- bash/templates/main/bash.bash
+++ bash/templates/main/bash.bash
@@ -5,4 +5,3 @@
 # All default Bash environment variables have been moved from 'bash.bash' to
 # 'zz_bash.bash'. Files in '/etc/profile.d/' are loaded in alphabetical order.
 # Changing the order helps to ensure important variables do not get overwritten.
-#
--- shell/templates/main/shell.sh
+++ shell/templates/main/shell.sh
@@ -5,4 +5,3 @@
 # All default Shell environment variables have been moved from 'shell.sh' to
 # 'z_shell.sh'. Files in '/etc/profile.d/' are loaded in alphabetical order.
 # Changing the order helps to ensure important variables do not get overwritten.
-#
